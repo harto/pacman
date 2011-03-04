103e1f9a3aa0201b744ebce1827bb29932e6feb3
Stuart Campbell <stuart@harto.org>
2011-03-04 12:06:37 +1100

    first cut of blinky implementation

--- pacman.js
+++ pacman.js
@@ -2,6 +2,9 @@
  * An HTML5 Pac-Man clone.
  * https://www.github.com/harto/pacman
  *
+ * Based on the game as described by The Pac-Man Dossier
+ * (http://home.comcast.net/~jpittman2/pacman/pacmandossier.html)
+ *
  * Requires: jQuery 1.4.2
  */
 
@@ -17,16 +20,60 @@ var TILE_SIZE = 8,
 
     MAX_SPEED = 1,
 
-    DEBUG = false,//true,
+    DEBUG = false,
 
     NORTH = 1,
     SOUTH = 2,
-    EAST = 3,
-    WEST = 4,
+    EAST = 4,
+    WEST = 16,
 
     lives,
     score,
     level;
+
+/// miscellany
+
+function toTileCoord(coord) {
+    var x = coord % TILE_SIZE;
+    // handle negative x-coord for tunnel
+    return x < 0 ? x + TILE_SIZE : x;
+}
+
+function toDx(direction) {
+    return direction === WEST ? -1 : direction === EAST ? 1 : 0;
+}
+function toDy(direction) {
+    return direction === NORTH ? -1 : direction === SOUTH ? 1 : 0;
+}
+
+function intersecting(ax, ay, aw, ah, bx, by, bw, bh) {
+    var ax2 = ax + aw, ay2 = ay + ah,
+        bx2 = bx + bw, by2 = by + bh;
+    return (// x-overlap
+            (((bx <= ax && ax <= bx2) || (bx <= ax2 && ax2 <= bx2)) ||
+             ((ax <= bx && bx <= ax2) || (ax <= bx2 && bx2 <= ax2))) &&
+           (// y-overlap
+            (((by <= ay && ay <= by2) || (by <= ay2 && ay2 <= by2))) ||
+             ((ay <= by && by <= ay2) || (ay <= by2 && by2 <= ay2))));
+}
+
+function distance(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
+
+function reverse(direction) {
+    return direction === NORTH ? SOUTH :
+           direction === SOUTH ? NORTH :
+           direction === EAST ? WEST :
+           EAST;
+}
+
+Array.prototype.remove = function (o) {
+    var i = this.indexOf(o);
+    if (i !== -1) {
+        this.splice(i, 1);
+    }
+};
 
 /// partial re-rendering
 
@@ -42,29 +89,7 @@ function invalidateScreen() {
     invalidateRegion(0, 0, SCREEN_W, SCREEN_H);
 }
 
-/// miscellany
-
-function toLocal(coord) {
-    return coord % TILE_SIZE;
-}
-
-function intersecting(ax, ay, aw, ah, bx, by, bw, bh) {
-    var ax2 = ax + aw, ay2 = ay + ah,
-        bx2 = bx + bw, by2 = by + bh;
-    return (// x-overlap
-            (((bx <= ax && ax <= bx2) || (bx <= ax2 && ax2 <= bx2)) ||
-             ((ax <= bx && bx <= ax2) || (ax <= bx2 && bx2 <= ax2))) &&
-           (// y-overlap
-            (((by <= ay && ay <= by2) || (by <= ay2 && ay2 <= by2))) ||
-             ((ay <= by && by <= ay2) || (ay <= by2 && by2 <= ay2))));
-}
-
-Array.prototype.remove = function (o) {
-    var i = this.indexOf(o);
-    if (i !== -1) {
-        this.splice(i, 1);
-    }
-};
+/// base class of most entities
 
 function Sprite() {}
 
@@ -76,10 +101,9 @@ Sprite.prototype = {
         invalidateRegion(this.x, this.y, this.w, this.h);
     },
     repaint: function (g, invalidated) {
-        var s = this;
         var invalid = invalidated.some(function (r) {
-            return s.intersects(r.x, r.y, r.w, r.h);
-        });
+            return this.intersects(r.x, r.y, r.w, r.h);
+        }, this);
         if (invalid) {
             this.draw(g);
         }
@@ -207,17 +231,39 @@ var maze = {
         }
     },
 
-    adjacentTiles: function (col, row) {
-        var adjacent = {};
-        adjacent[NORTH] = this.canEnter(col, row - 1);
-        adjacent[SOUTH] = this.canEnter(col, row + 1);
-        adjacent[WEST] = this.canEnter(col - 1, row);
-        adjacent[EAST] = this.canEnter(col + 1, row);
-        return adjacent;
+    enterable: function (col, row) {
+        return this.layout[row][col] !== '#';
     },
 
-    canEnter: function (col, row) {
-        return this.layout[row][col] !== '#';
+    // Return a number that is the bitwise-OR of directions in which an actor
+    // may exit a given tile.
+    exitsFrom: function (col, row) {
+        if (col < 0 || COLS <= col) {
+            // in tunnel
+            return EAST | WEST;
+        } else {
+            return (this.enterable(col, row - 1) ? NORTH : 0) |
+                   (this.enterable(col, row + 1) ? SOUTH : 0) |
+                   (this.enterable(col - 1, row) ? WEST : 0) |
+                   (this.enterable(col + 1, row) ? EAST : 0);
+        }
+    },
+
+    // check if tile falls within one of two zones in which ghosts are
+    // prohibited from turning north
+    northDisallowed: function (col, row) {
+        return (12 <= col && col <= 15) && (row === 14 || row === 26);
+    },
+
+    inTunnel: function (col, row) {
+        return row === 17 && (col <= 4 || 23 <= col);
+    },
+
+    // return the tunnel reentry column corresponding to a given column (if any)
+    reentryCol: function (col) {
+        return col === -3 ? COLS + 1 :
+               col === COLS + 2 ? -2 :
+               null;
     },
 
     dotAt: function (col, row) {
@@ -235,14 +281,25 @@ var maze = {
         if (dot instanceof Energiser) {
             this.energisers.remove(dot);
         }
+
+        if (this.dots.length === 174 || this.dots.length === 74) {
+            // TODO: add fruit
+        }
+    },
+
+    isEmpty: function () {
+        return this.dots.length === 0;
     },
 
     repaint: function (g, invalidated) {
-        var bg = this.bg;
         invalidated.forEach(function (r) {
-            var x = r.x, y = r.y, w = r.w, h = r.h;
-            g.drawImage(bg, x, y, w, h, x, y, w, h);
-        });
+            // handle out-of-bounds indexes
+            var x = Math.max(0, r.x), y = r.y,
+                w = r.w - (x - r.x), h = r.h;
+            if (w > 0) {
+                g.drawImage(this.bg, x, y, w, h, x, y, w, h);
+            }
+        }, this);
         this.dots.forEach(function (d) {
             d.repaint(g, invalidated);
         });
@@ -257,43 +314,61 @@ var maze = {
 
 /// actors
 
-var pacman = new Sprite();
+function Actor() {}
 
-pacman.w = pacman.h = 1.5 * TILE_SIZE;
-pacman.speed = 0.8 * MAX_SPEED;
+Actor.prototype = new Sprite();
 
-pacman.reset = function () {
-    this.x = 14 * TILE_SIZE - this.w / 2;
-    this.y = 26 * TILE_SIZE - (this.h - TILE_SIZE) / 2;
-    this.updateCentrepoint();
-    this.updateTileLocation();
-    this.direction = EAST;
+Actor.prototype.calcCentreX = function () {
+    return this.x + this.w / 2;
+};
+Actor.prototype.calcCentreY = function () {
+    return this.y + this.h / 2;
+};
+Actor.prototype.calcCol = function () {
+    return Math.floor(this.calcCentreX() / TILE_SIZE);
+};
+Actor.prototype.calcRow = function () {
+    return Math.floor(this.calcCentreY() / TILE_SIZE);
+};
+Actor.prototype.calcTileX = function () {
+    return toTileCoord(this.calcCentreX());
+};
+Actor.prototype.calcTileY = function () {
+    return toTileCoord(this.calcCentreY());
 };
 
-pacman.updateCentrepoint = function () {
-    this.cx = this.x + this.w / 2;
-    this.cy = this.y + this.h / 2;
+Actor.prototype.enteringTile = function () {
+    var x = this.calcTileX(), y = this.calcTileY();
+    return (this.direction === EAST && x === 0) ||
+           (this.direction === WEST && x === TILE_SIZE - 1) ||
+           (this.direction === SOUTH && y === 0) ||
+           (this.direction === NORTH && y === TILE_SIZE - 1);
 };
 
-pacman.updateTileLocation = function () {
-    this.col = Math.floor((this.x + this.w / 2) / TILE_SIZE);
-    this.row = Math.floor((this.y + this.h / 2) / TILE_SIZE);
-    this.adjacentTiles = maze.adjacentTiles(this.col, this.row);
-};
-
-pacman.draw = function (g) {
+Actor.prototype.draw = function (g) {
     g.save();
     // FIXME
-    g.fillStyle = 'yellow';
+    g.fillStyle = this.colour;
     g.fillRect(this.x, this.y, this.w, this.h);
     g.restore();
 };
 
-pacman.enteringTile = function () {
-    return (this.direction === EAST && toLocal(this.cx) === 0) ||
-           (this.direction === WEST && toLocal(this.cx) === TILE_SIZE - 1) ||
-           (this.direction === SOUTH && toLocal(this.cy) === 0) ||
-           (this.direction === NORTH && toLocal(this.cy) === TILE_SIZE - 1);
+/// pacman
+
+var pacman = new Actor();
+
+pacman.w = pacman.h = 1.5 * TILE_SIZE;
+pacman.startX = 14 * TILE_SIZE - pacman.w / 2;
+pacman.startY = 26 * TILE_SIZE - (pacman.h - TILE_SIZE) / 2;
+// FIXME
+pacman.colour = 'yellow';
+
+//pacman.speed = 0.8 * MAX_SPEED;
+
+pacman.reset = function () {
+    this.x = this.startX;
+    this.y = this.startY;
+    this.direction = WEST;
 };
 
 pacman.update = function () {
@@ -309,24 +384,36 @@ pacman.update = function () {
         this.move(this.direction);
     }
 
-    if (this.enteringTile()) {
-        this.updateTileLocation();
-        var dot = maze.dotAt(this.col, this.row);
-        if (dot) {
-            score += dot.value;
-            maze.remove(dot);
-            this.wait = dot.delay;
-            if (dot instanceof Energiser) {
-                // TODO: energise
-            }
-        }
+    if (!this.enteringTile()) {
+        return;
     }
+
+    var col = this.calcCol();
+    var reentryCol = maze.reentryCol(col);
+    if (reentryCol) {
+        this.x = TILE_SIZE * reentryCol;
+        return;
+    }
+
+    var dot = maze.dotAt(col, this.calcRow());
+    if (dot) {
+        score += dot.value;
+        this.wait = dot.delay;
+        if (dot instanceof Energiser) {
+            // TODO: energise
+        }
+        maze.remove(dot);
+    }
+
+    // TODO: ghost collision check
 };
 
 pacman.move = function (direction) {
     var dx = 0;
     var dy = 0;
     var exiting;
+    var cx = this.calcCentreX();
+    var cy = this.calcCentreY();
     var centre = TILE_SIZE / 2;
 
     // Move in the given direction iff before tile centrepoint or
@@ -334,36 +421,179 @@ pacman.move = function (direction) {
 
     if (direction === EAST) {
         dx = 1;
-        exiting = toLocal(this.cx + dx) > centre;
+        exiting = toTileCoord(cx + dx) > centre;
     } else if (direction === WEST) {
         dx = -1;
-        exiting = toLocal(this.cx + dx) < centre;
+        exiting = toTileCoord(cx + dx) < centre;
     } else if (direction === SOUTH) {
         dy = 1;
-        exiting = toLocal(this.cy + dy) > centre;
+        exiting = toTileCoord(cy + dy) > centre;
     } else if (direction === NORTH) {
         dy = -1;
-        exiting = toLocal(this.cy + dy) < centre;
+        exiting = toTileCoord(cy + dy) < centre;
     }
 
-    if (!exiting || this.adjacentTiles[direction]) {
-        this.invalidate();
-
-        // pre/post-turning
-        if (dx) {
-            var localY = toLocal(this.cy);
-            dy = localY > centre ? -1 : localY < centre ? 1 : 0;
-        } else if (dy) {
-            var localX = toLocal(this.cx);
-            dx = localX > centre ? -1 : localX < centre ? 1 : 0;
-        }
-
-        this.x += dx;
-        this.y += dy;
-        this.updateCentrepoint();
-        return true;
+    if (exiting && !(direction & maze.exitsFrom(this.calcCol(), this.calcRow()))) {
+        return false;
     }
+
+    this.invalidate();
+
+    // cornering
+    if (dx) {
+        var localY = toTileCoord(cy);
+        dy = localY > centre ? -1 : localY < centre ? 1 : 0;
+    } else if (dy) {
+        var localX = toTileCoord(cx);
+        dx = localX > centre ? -1 : localX < centre ? 1 : 0;
+    }
+
+    this.x += dx;
+    this.y += dy;
+    return true;
 };
+
+/// ghosts
+
+function Ghost(name, startX, startY, scatterCol, scatterRow) {
+    this.name = name;
+
+    this.w = this.h = Ghost.SIZE;
+    this.startX = startX;
+    this.startY = startY;
+
+    this.scatterCol = scatterCol;
+    this.scatterRow = scatterRow;
+}
+
+Ghost.SIZE = TILE_SIZE * 1.5;
+
+Ghost.CHASE = 1;
+Ghost.SCATTER = 2;
+Ghost.FRIGHTENED = 3;
+//Ghost.MODE =
+
+Ghost.prototype = new Actor();
+
+Ghost.prototype.toString = function () {
+    return this.name;
+};
+
+Ghost.prototype.reset = function () {
+    this.x = this.startX;
+    this.y = this.startY;
+    // FIXME
+    this.direction = WEST;
+    this.nextDirection = this.calcNextDirection();
+};
+
+Ghost.prototype.update = function () {
+    this.invalidate();
+    this.x += toDx(this.direction);
+    this.y += toDy(this.direction);
+
+    var col = this.calcCol();
+    if (maze.inTunnel(col, this.calcRow())) {
+        // TODO: reduce speed
+
+        var reentryCol = maze.reentryCol(col);
+        if (reentryCol) {
+            this.x = TILE_SIZE * reentryCol;
+        }
+    }
+
+    // Moves are computed one tile in advance - when a ghost reaches a tile, it
+    // inspects the next tile in its current direction and determines which way
+    // to go when it arrives at that tile.
+    //
+    // According to the Pac-Man Dossier, "arriving" at a tile occurs at the
+    // moment an actor's centrepoint crosses the tile boundary. For the purposes
+    // of this pathfinding algorithm, it is defined as the moment a ghost reaches
+    // the centre of the tile. This simplifies the lookahead logic. Hopefully it
+    // doesn't significantly affect gameplay.
+
+    var centre = TILE_SIZE / 2;
+    if (this.calcTileX() !== centre || this.calcTileY() !== centre) {
+        return;
+    }
+
+    this.direction = this.nextDirection;
+    this.nextDirection = this.calcNextDirection();
+};
+
+// calculate direction to be taken when next tile is reached
+Ghost.prototype.calcNextDirection = function () {
+    var nextCol = this.calcCol() + toDx(this.direction);
+    var nextRow = this.calcRow() + toDy(this.direction);
+
+    var exits = maze.exitsFrom(nextCol, nextRow);
+    // exclude illegal moves
+    exits &= ~reverse(this.direction);
+    if (maze.northDisallowed(nextCol, nextRow)) {
+        exits &= ~NORTH;
+    }
+    // check for single available exit
+    if (exits === NORTH || exits === SOUTH || exits === WEST || exits === EAST) {
+        return exits;
+    }
+
+    var target = this.calcTarget();
+
+    function distanceFrom(col, row) {
+        return distance(col, row, target.col, target.row);
+    }
+
+    var directions = [];
+
+    // Add candidates in tie-break order
+    if (exits & NORTH) {
+        directions.push({ direction: NORTH,
+                          dist: distanceFrom(nextCol, nextRow - 1) });
+    }
+    if (exits & WEST) {
+        directions.push({ direction: WEST,
+                          dist: distanceFrom(nextCol - 1, nextRow) });
+    }
+    if (exits & SOUTH) {
+        directions.push({ direction: SOUTH,
+                          dist: distanceFrom(nextCol, nextRow + 1) });
+    }
+    if (exits & EAST) {
+        directions.push({ direction: EAST,
+                          dist: distanceFrom(nextCol + 1, nextRow) });
+    }
+
+    directions.sort(function (a, b) {
+        return a.dist - b.dist;
+    });
+
+    return directions[0].direction;
+};
+
+/// blinky
+
+var blinky = new Ghost('Blinky',
+                       14 * TILE_SIZE - Ghost.SIZE / 2,
+                       15 * TILE_SIZE - (Ghost.SIZE + TILE_SIZE) / 2,
+                       25, 0);
+// FIXME
+blinky.colour = 'red';
+
+blinky.calcTarget = function () {
+    return { col: pacman.calcCol(), row: pacman.calcRow() };
+};
+
+// /// pinky
+
+// var pinky = new Ghost();
+
+// /// inky
+
+// var inky = new Ghost();
+
+// /// clyde
+
+// var clyde = new Ghost();
 
 /// scoreboard
 
@@ -393,7 +623,7 @@ pacman.move = function (direction) {
 
 /// engine
 
-var entities = [maze, pacman],
+var entities = [maze, pacman, blinky/*, pinky, inky, clyde*/],
     ctx,
 
     // game states
@@ -434,6 +664,10 @@ function update() {
         entities.forEach(function (a) {
             a.update();
         });
+        if (maze.isEmpty()) {
+            // FIXME: delay this
+            levelUp();
+        }
     // } else if (state === State.REINSERT) {
     //     if (new Date() - timeOfDeath >= Ball.REINSERT_DELAY) {
     //         ball.reset();
@@ -458,6 +692,7 @@ function loop() {
 }
 
 function levelUp() {
+    ++level;
     entities.forEach(function (e) {
         e.reset();
     });
