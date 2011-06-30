9313d223b9de7e62982b65faa6471a61597b7545
Stuart Campbell <stuart@harto.org>
2011-06-30 11:18:46 +1000

    extract project-independent stuff to util.js

--- src/core.js
+++ src/core.js
@@ -3,7 +3,7 @@
  */
 
 /*jslint bitwise: false */
-/*global $, console */
+/*global $, console, noop, format, copy, values */
 
 var TILE_SIZE = 8,
     TILE_CENTRE = TILE_SIZE / 2,
@@ -28,16 +28,16 @@ var TILE_SIZE = 8,
 
 /// miscellany
 
-function noop() {}
-
 function toCol(x) {
     return Math.floor(x / TILE_SIZE);
 }
+
 var toRow = toCol;
 
 function toDx(direction) {
     return direction === WEST ? -1 : direction === EAST ? 1 : 0;
 }
+
 function toDy(direction) {
     return direction === NORTH ? -1 : direction === SOUTH ? 1 : 0;
 }
@@ -56,6 +56,7 @@ function reverse(direction) {
 function toTicks(seconds) {
     return Math.round(seconds * UPDATE_HZ);
 }
+
 function toSeconds(ticks) {
     return ticks / UPDATE_HZ;
 }
@@ -64,67 +65,10 @@ function toOrdinal(direction) {
     return Math.log(direction) / Math.log(2);
 }
 
-// custom printf-style formatting
-function formatNumber(num, fmt) {
-    num += '';
-    if (!fmt) {
-        return num;
-    }
-    var numParts = num.split('.'), dec = numParts[0], frac = numParts[1] || '',
-        fmtParts = fmt.split('.'), nDec = fmtParts[0], nFrac = fmtParts[1];
-    if (nFrac === undefined) {
-        return dec.substring(0, nDec);
-    } else {
-        while (frac.length < nFrac) {
-            frac += '0';
-        }
-        return (nDec === '' ? dec : dec.substring(0, nDec)) + '.' +
-               frac.substring(0, nFrac);
-    }
-}
-function format(msg/*, args*/) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return (msg + '').replace(/%(s|[0-9]?\.?[0-9]?n)/g, function (_, code) {
-        var arg = args.shift();
-        switch (code.charAt(code.length - 1)) {
-        case 's':
-            return arg;
-        case 'n':
-            return formatNumber(arg, code.substring(0, code.length - 1));
-        default:
-            throw new Error('bad format code: ' + code);
-        }
-    });
-}
-
 function debug(/*msg, args*/) {
     if (DEBUG) {
         console.log(format.apply(this, arguments));
     }
-}
-
-// Would be nice to extend Object with these, but it breaks jQuery
-
-function keys(o) {
-    var ks = [];
-    for (var k in o) {
-        if (o.hasOwnProperty(k)) {
-            ks.push(k);
-        }
-    }
-    return ks;
-}
-
-function values(o) {
-    return keys(o).map(function (k) {
-        return o[k];
-    }, this);
-}
-
-function copy(from, to) {
-    keys(from).forEach(function (k) {
-        to[k] = from[k];
-    });
 }
 
 /// native object extensions
@@ -156,16 +100,6 @@ Array.prototype.doAll = function (fName /*, args...*/) {
         e[fName].apply(e, args);
     });
 };
-
-Math.sign = function (x) {
-    return x < 0 ? -1 : x > 0 ? 1 : 0;
-};
-
-// remove fractional part of number
-Math.trunc = function (x) {
-    return Math.sign(x) * Math.floor(Math.abs(x));
-};
-
 /// graphics
 
 function ScreenBuffer(w, h) {
---
+++ src/util.js
@@ -0,0 +1,104 @@
+/*
+ * Miscellaneous project-independent utilities
+ */
+
+// format a number (fmt = '3.1', '.4', etc)
+function formatNumber(num, fmt) {
+    num += '';
+    if (!fmt) {
+        return num;
+    }
+    var numParts = num.split('.'), dec = numParts[0], frac = numParts[1] || '',
+        fmtParts = fmt.split('.'), nDec = fmtParts[0], nFrac = fmtParts[1];
+    if (nFrac === undefined) {
+        return dec.substring(0, nDec);
+    } else {
+        while (frac.length < nFrac) {
+            frac += '0';
+        }
+        return (nDec === '' ? dec : dec.substring(0, nDec)) + '.' +
+               frac.substring(0, nFrac);
+    }
+}
+
+// printf-style formatting
+function format(msg/*, args*/) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return (msg + '').replace(/%(s|[0-9]?\.?[0-9]?n)/g, function (_, code) {
+        var arg = args.shift();
+        switch (code.charAt(code.length - 1)) {
+        case 's':
+            return arg;
+        case 'n':
+            return formatNumber(arg, code.substring(0, code.length - 1));
+        default:
+            throw new Error('bad format code: ' + code);
+        }
+    });
+}
+
+function noop() {}
+
+function bind(o, f) {
+    return function (/*...*/) {
+        f.call(o, arguments);
+    };
+}
+
+function alias(o, fName) {
+    var f = o[fName];
+    return f ? bind(o, f) : noop;
+}
+
+function keys(o) {
+    var ks = [];
+    for (var k in o) {
+        if (o.hasOwnProperty(k)) {
+            ks.push(k);
+        }
+    }
+    return ks;
+}
+
+function values(o) {
+    return keys(o).map(function (k) {
+        return o[k];
+    }, this);
+}
+
+function copy(from, to) {
+    keys(from).forEach(function (k) {
+        to[k] = from[k];
+    });
+}
+
+// unobtrusive Math extensions
+
+Math.sign = function (x) {
+    return x < 0 ? -1 : x > 0 ? 1 : 0;
+};
+
+// remove fractional part of number
+Math.trunc = function (x) {
+    return Math.sign(x) * Math.floor(Math.abs(x));
+};
+
+// Array helpers
+
+// remove element from array in linear time
+function remove(array, o) {
+    var i = array.indexOf(o);
+    if (i !== -1) {
+        array.splice(i, 1);
+    }
+}
+
+// return first element matching pred in linear time
+function first(array, pred) {
+    for (var i = 0; i < array.length; i++) {
+        var x = array[i];
+        if (pred(x)) {
+            return x;
+        }
+    }
+}
